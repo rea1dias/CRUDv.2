3a52ac6ea2350f3168fca312c53c42ef08e278d9
Dias Rakhman <rahmanovdias13@gmail.com>
2024-09-21 23:16:43 +0500

    Home page added

--- src/main/java/com/example/Simple/CRUD/Project/Controller/HomeController.java
+++ src/main/java/com/example/Simple/CRUD/Project/Controller/HomeController.java
@@ -1,14 +1,13 @@
 package com.example.Simple.CRUD.Project.Controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class TestController {
+public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/products")
     public String index() {
         return "index";
     }
